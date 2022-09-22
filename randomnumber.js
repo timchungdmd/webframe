ddbeaee6bfcdc6718c9608a89c90fdc3b42fe99b
Tim Chung <timchung.dmd@gmail.com>
2022-09-21 19:42:29 -0700

    modified:   randomnumber.js

--- randomnumber.js
+++ randomnumber.js
@@ -12,6 +12,7 @@ function guessNumber() {
         else if(number===randomnumbergenerator){
         alert('Congratulations! You guessed the correct number.');
         }
+    alert('You used all your chances. Please try again later');    
     }
    
   }
