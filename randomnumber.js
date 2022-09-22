59841191517e3cf860ca508454f8e1f35433808d
Tim Chung <timchung.dmd@gmail.com>
2022-09-21 19:06:57 -0700

    new file:   randomnumber.js

---
+++ randomnumber.js
@@ -0,0 +1,13 @@
+function guessNumber() {
+    const randomnumbergenerator = Math.floor(Math.random() * 10) + 1;
+
+    let number = parseInt(prompt('Pick a number between 1 and 10: '));
+    while(number !== randomnumbergenerator) {
+        number = parseInt(prompt('Wrong number, pick another number between 1 and 10: '));
+    }
+    if(number == random) {
+        console.log('You guessed the correct number.');
+    }
+
+  }
+guessNumber();
