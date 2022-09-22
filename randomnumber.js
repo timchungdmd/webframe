40b4f717516b7f6710f446e8e4ccecdd1e5e4d9e
Tim Chung <timchung.dmd@gmail.com>
2022-09-21 20:18:29 -0700

    modified:   randomnumber.js

--- randomnumber.js
+++ randomnumber.js
@@ -1,13 +1,11 @@
 function guessNumber() {
     const randomnumbergenerator = Math.floor(Math.random() * 10) + 1;
-    let number = parseInt(prompt('Pick a number between 1 and 10. You only get 5 chances: '));
+    let number = parseInt(prompt('Pick a number between 1 and 10.: '));
     
-    while(number!==randomnumbergenerator){
+    if(number!==randomnumbergenerator){
         number = parseInt(prompt('Wrong number, pick another number between 1 and 10: '));
         }
     if(number===randomnumbergenerator){
         alert('Congratulations! You guessed the correct number.');
         }   
     }
-   
-guessNumber();
