0420d21428876ce881c1e840280c683a8966dff0
Tim Chung <timchung.dmd@gmail.com>
2022-09-23 11:42:44 -0700

    new file:   getpokeball.js

---
+++ getpokeball.js
@@ -0,0 +1,9 @@
+function getPokeball(){
+    const myImages = document.images;
+    let text = "";
+    for (let i = 0; i <randomnumber; i++) {
+      console.log('test');
+    }
+    document.getElementById("demo").innerHTML = text;
+    }
+    getPokeball();
