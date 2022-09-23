b6ea44daf826c653892bc0553e309c2758fc9b04
Tim Chung <timchung.dmd@gmail.com>
2022-09-23 12:33:59 -0700

    modified:   getpokeball.js

--- getpokeball.js
+++ getpokeball.js
@@ -3,8 +3,8 @@ function getPokeball(){
     let randomnumber=parseInt(prompt('How many pokeballs do you need?'));
     let text = "";
     for (let i = 0; i <randomnumber; i++) {
-      console.log('test');
+      text=console.log('test');
     }
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("pokeball").innerHTML = text;
     }
     getPokeball();
