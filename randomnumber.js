4a27cb2754c478226c6df7989a5113de8904a67c
Tim Chung <timchung.dmd@gmail.com>
2022-09-23 11:25:50 -0700

    modified:   randomnumber.js

--- randomnumber.js
+++ randomnumber.js
@@ -1,15 +1,26 @@
 function guessNumber() {
-    const randomnumbergenerator = Math.floor(Math.random() * 10) + 1;
+    const randomnumber = Math.floor(Math.random() * 10) + 1;
 
     let number = parseInt(prompt('Guess how many pokemons you must capture to go to the next level?(between 1 and 10) ')
     );
     
-    while(number!==randomnumbergenerator){
+    while(number!==randomnumber){
         number = parseInt(prompt('Wrong! Guess again: '));
         }
-    if(number===randomnumbergenerator){
+    if(number===randomnumber){
         alert(`Congratulations! you have to capture ${randomnumbergenerator} pokemons`);
         }
        
     }
     guessNumber();
+
+
+function getPokeball(){
+const myImages = document.images;
+let text = "";
+for (let i = 0; i <randomnumber; i++) {
+  console.log('test');
+}
+document.getElementById("demo").innerHTML = text;
+}
+getPokeball();
