1e805c61cb4c91fccc4b70c4d951db04d08aa45f
Tim Chung <timchung.dmd@gmail.com>
2022-09-23 16:01:41 -0700

    modified:   getpokeball.js

--- getpokeball.js
+++ getpokeball.js
@@ -4,8 +4,8 @@ function getPokeball(){
     let text = "";
     if (isNaN(randomnumber)){
         parseInt(prompt('You can only type a number, how many pokeballs do you need'));
-    }else if(randomnumber>10);{
-        parseInt(prompt('You can\'t have more than 10 pokeballs. How many pokeballs do you need'))
+    }else if(randomnumber>10){
+        parseInt(prompt('You can\'t have more than 10 pokeballs. How many pokeballs do you need'));
     }
     else if(typeof randomnumber=='number' && randomnumber<=10){
 
