655b3c8cc11b1eec6018a23a1e42d75253c9d69d
Tim Chung <timchung.dmd@gmail.com>
2022-09-22 23:31:00 -0700

    modified:   randomnumber.js

--- randomnumber.js
+++ randomnumber.js
@@ -19,7 +19,7 @@ guessNumber();
 
 
 function showPicture() {
-  var sourceOfPicture = "http://img.tesco.com/Groceries/pi/118/5000175411118/IDShot_90x90.jpg";
+  var sourceOfPicture = "pokeball.png";
   var img = document.getElementById('bigpic')
   img.src = sourceOfPicture.replace('90x90', '225x225');
   img.style.display = "block";
